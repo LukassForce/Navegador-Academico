d958c5598698cc437fe820e4a9fa2e54a75c6f3b
Lucas Cruz <lcruz@chilexpress.cl>
2022-07-03 23:29:36 -0400

    base de datos

--- Database/navegador.sql
+++ Database/navegador.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2022 at 05:09 AM
+-- Generation Time: Jul 04, 2022 at 05:28 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -36,6 +36,13 @@ CREATE TABLE `justificacion` (
   `mensaje` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `justificacion`
+--
+
+INSERT INTO `justificacion` (`id`, `nombre`, `apellido`, `curso`, `asunto`, `mensaje`) VALUES
+(30, '3123123', '123123', '123123', '123123', '123123');
+
 -- --------------------------------------------------------
 
 --
@@ -44,9 +51,23 @@ CREATE TABLE `justificacion` (
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
-  `nameId` int(11) NOT NULL,
-  `contrasena` int(11) NOT NULL
+  `rut` varchar(11) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nombre` varchar(32) NOT NULL,
+  `apellido` varchar(32) NOT NULL,
+  `curso` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `rut`, `password`, `nombre`, `apellido`, `curso`) VALUES
+(11, '20529577-1', '$2a$10$cLfoQjVokhnw.OygwukTh.Yrx', 'Lucas', 'Cruz', '3ero Medio'),
+(12, '20529577-1', '$2a$10$8otSZsjL46nQJ4cphBknbOJr2', 'Lucas', 'Cruz', '3ero Medio'),
+(13, '20529577-2', '$2a$10$ESP8cXOfmMbKdaMyiNlAA.2im', 'Lucas', 'Cruz', '3ero Medio'),
+(14, '20529577-3', '$2a$10$8Hihw6Rk/0KpfkbEbk4cLOFyo', 'Lucas', 'Cruz', '3ero Medio'),
+(15, '20529577-4', '$2a$10$TIFz2Ncypc6hzUJYfDnLvOmxk', 'Palta', 'mayo', '3ero Medio');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +83,8 @@ ALTER TABLE `justificacion`
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Nombre` (`nombre`,`apellido`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -72,13 +94,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `justificacion`
 --
 ALTER TABLE `justificacion`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
